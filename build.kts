d6f18859e6f47441476f492d93aea0465a98854e
codexcoder21 <codexcoder21@users.noreply.github.com>
2026-07-22 04:12:58 +0000

    Work around Kompile clock ABI skew

--- build.kts
+++ build.kts
@@ -12,7 +12,7 @@ val dependencies = listOf(
     MavenPrebuilt2("simplefilesystem:simplefilesystem-api:0.1.0"),
     MavenPrebuilt2("blobstore.api:blobstore-api:0.0.2"),
     MavenPrebuilt2("sql:sql-api:0.0.1"),
-    MavenPrebuilt2("community.kotlin.clocks.simple:community-kotlin-clocks-simple:0.0.7"),
+    MavenPrebuilt2("community.kotlin.clocks.simple:community-kotlin-clocks-simple:0.0.3"),
     MavenPrebuilt2("com.squareup.okio:okio-jvm:3.4.0"),
     MavenPrebuilt2("org.jetbrains.kotlin:kotlin-stdlib:1.9.22"),
     MavenPrebuilt2("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.22"),
--- tests/testFilesystemLifecycle.kts
+++ tests/testFilesystemLifecycle.kts
@@ -4,7 +4,7 @@
 @file:WithArtifact("cockroachdb.testharness:cockroachdb-test-harness:0.0.4")
 @file:WithArtifact("sql:sql-api:0.0.1")
 @file:WithArtifact("sql:sql:0.0.2")
-@file:WithArtifact("community.kotlin.clocks.simple:community-kotlin-clocks-simple:0.0.7")
+@file:WithArtifact("community.kotlin.clocks.simple:community-kotlin-clocks-simple:0.0.3")
 @file:WithArtifact("org.postgresql:postgresql:42.6.0")
 @file:WithArtifact("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 @file:WithArtifact("org.jetbrains.kotlin:kotlin-test:1.9.22")
@@ -14,6 +14,7 @@ import build.kotlin.withartifact.WithArtifact
 import cockroachdb.testharness.LocalCockroachCluster
 import community.kotlin.blobstore.inmemory.InMemoryBlobstoreService
 import community.kotlin.clocks.simple.ManualClock
+import community.kotlin.clocks.simple.SystemClock
 import java.util.UUID
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -22,7 +23,7 @@ import simplefilesystem.FilesystemNotFoundException
 import sql.Database
 
 fun testFilesystemLifecycle() {
-    val cluster = LocalCockroachCluster().start()
+    val cluster = LocalCockroachCluster(clock = SystemClock()).start()
     try {
         val database = Database("org.postgresql.Driver", cluster.jdbcUrl(), cluster.username, cluster.password)
         try {
